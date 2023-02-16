d88d542f98052bac77f22a95235a7716e65b3b07
andrescaro16 <julicaro2003@gmail.com>
2023-02-16 00:24:08 -0500

    Search bar base DEV 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,6 @@ import './Assets/CSS/App.css';
 import Search from './Components/Search';
 
 
-function saludo(name, age) {
-  
-  let presentation = (
-    <div>
-      <h1> Hola, soy {name}</h1>
-      <h2>Tengo {age} años</h2>
-    </div>
-  );
-
-  return presentation;
-}
-
-
 function App() {
 
   return (
